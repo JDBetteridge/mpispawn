492b33bd930392d9c10ac3d664e53e7c0e429fdc
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2025-11-11 00:15:55 +0000

    Fix for gathering return statuses

--- mpispawn/wrapper.py
+++ mpispawn/wrapper.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 
+import numpy as np
 from mpi4py import MPI
 
 
@@ -8,13 +9,19 @@ def wrapper():
     """Thin wrapper to instantiate MPI and handle return codes"""
     comm = MPI.Comm.Get_parent()
 
-    out = subprocess.run(sys.argv[1:])
+    try:
+        out = subprocess.run(sys.argv[1:])
+        return_code = out.returncode
+    except FileNotFoundError:
+        return_code = 127
 
     if comm != MPI.COMM_NULL:
-        comm.gather(out.returncode, root=0)
+        buffer = np.empty((1,), dtype=np.int8)
+        buffer[0] = return_code
+        comm.Gather(buffer, None, root=0)
         comm.Disconnect()
 
-    return out.returncode
+    return return_code
 
 
 if __name__ == '__main__':
